741a4bd97e904d0587aa54e3f95afed5ab69c264
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2011-10-27 22:45:23 +0900

    add highlight

--- neet/assets/js/main.js
+++ neet/assets/js/main.js
@@ -26,6 +26,9 @@ $(function(){
 		updateWindowSize();
 	});
 
+	hljs.tabReplace = '    '; // 4 spaces
+	hljs.initHighlightingOnLoad();
+	$('code').each(function(i, e) {hljs.highlightBlock(e, '    ')});
 
 	function prev(p) {
 		if (p > 0) p = move(p - 1); 
